dcc3b0b48388374680fcd7e047ca9173f304942b
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-11-14 06:54:54 +0000

    Added generateSignals, generateSignalsForAllLiveCells, countSignals functions along with the test cases
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign4/Scala@3231 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/GameOfLife.scala
+++ src/GameOfLife.scala
@@ -1,6 +1,20 @@
 object GameOfLife{ 
 
-	def isAlive(alive: Boolean, numberOfLiveNeighbors: Int) =
-	  alive && numberOfLiveNeighbors == 2 || numberOfLiveNeighbors == 3
-
+	def isAlive(alive: Boolean, numberOfLiveNeighbors: Int) = {
+	  (alive && numberOfLiveNeighbors == 2) || numberOfLiveNeighbors == 3
+	}
+	
+	def generateSignals(liveCell: (Int, Int)) = {
+	  val (x, y) = liveCell
+	  List((x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1),
+			(x, y + 1), (x + 1, y - 1), (x + 1, y), (x + 1, y + 1))
+	}
+	
+	def generateSignalsForAllLiveCells(liveCells: List[(Int, Int)]) = {
+	  liveCells.map(generateSignals).reduce((x, y) => x ::: y)
+	}
+	
+	def countSignals(signals: List[(Int, Int)]) = {
+	  signals.groupBy(cell => cell).mapValues(_.length)
+	}
 }
--- test/GameOfLifeTest.scala
+++ test/GameOfLifeTest.scala
@@ -6,51 +6,115 @@ import org.junit.runner.RunWith
 class GameOfLifeTest extends FunSuite {
 
 	test("Live cell one neighbor") {
-		val expectedResult = false
-		val actualResult = GameOfLife.isAlive(true, 1)
-		assert(actualResult == expectedResult)
+	  val expectedResult = false
+	  val actualResult = GameOfLife.isAlive(true, 1)
+	  assert(actualResult == expectedResult)
 	}
 	
 	test("Live cell four neighbor") {
-		val expectedResult = false
-		val actualResult = GameOfLife.isAlive(true, 4)
-		assert(actualResult == expectedResult)
+	  val expectedResult = false
+	  val actualResult = GameOfLife.isAlive(true, 4)
+	  assert(actualResult == expectedResult)
 	}
 	
 	test("Live cell two neighbor") {
-		val expectedResult = true
-		val actualResult = GameOfLife.isAlive(true, 2)
-		assert(actualResult == expectedResult)
+	  val expectedResult = true
+	  val actualResult = GameOfLife.isAlive(true, 2)
+	  assert(actualResult == expectedResult)
 	}
 	
 	test("Live cell three neighbor") {
-		val expectedResult = true
-		val actualResult = GameOfLife.isAlive(true, 3)
-		assert(actualResult == expectedResult)
+	  val expectedResult = true
+	  val actualResult = GameOfLife.isAlive(true, 3)
+	  assert(actualResult == expectedResult)
 	}
 	
 	test("Dead cell one neighbor") {
-		val expectedResult = false
-		val actualResult = GameOfLife.isAlive(false, 1)
-		assert(actualResult == expectedResult)
+	  val expectedResult = false
+	  val actualResult = GameOfLife.isAlive(false, 1)
+	  assert(actualResult == expectedResult)
 	}
 	
 	test("Dead cell four neighbor") {
-		val expectedResult = false
-		val actualResult = GameOfLife.isAlive(false, 4)
-		assert(actualResult == expectedResult)
+	  val expectedResult = false
+	  val actualResult = GameOfLife.isAlive(false, 4)
+  	  assert(actualResult == expectedResult)
 	}
 	
 	test("Dead cell two neighbor") {
-		val expectedResult = false
-		val actualResult = GameOfLife.isAlive(false, 2)
-		assert(actualResult == expectedResult)
+	  val expectedResult = false
+	  val actualResult = GameOfLife.isAlive(false, 2)
+  	  assert(actualResult == expectedResult)
 	}
 	
 	test("Dead cell three neighbor") {
-		val expectedResult = true
-		val actualResult = GameOfLife.isAlive(false, 3)
-		assert(actualResult == expectedResult)
+	  val expectedResult = true
+	  val actualResult = GameOfLife.isAlive(false, 3)
+	  assert(actualResult == expectedResult)
+	}
+	
+	test("Generate Signals for cell (2, 3)") {
+	  val expectedResult = List((1, 2), (1, 3), (1, 4), (2, 2),
+		(2, 4), (3, 2), (3, 3), (3, 4))
+	  val actualResult = GameOfLife.generateSignals((2, 3))
+	  assert(actualResult == expectedResult)
+	}
+	
+	test("Generate Signals for cell (1, 4)") {
+	  val expectedResult = List((0, 3), (0, 4), (0, 5), (1, 3), 
+		(1, 5), (2, 3), (2, 4), (2, 5))
+	  val actualResult = GameOfLife.generateSignals((1, 4))
+	  assert(actualResult == expectedResult)
+	}
+	
+	test("Generate Signals for cell (0, 0)") {
+	  val expectedResult = List((-1, -1), (-1, 0), (-1, 1), (0, -1), 
+		(0, 1), (1, -1), (1, 0), (1, 1))
+	  val actualResult = GameOfLife.generateSignals((0, 0))
+	  assert(actualResult == expectedResult)
+	}
+	
+	test("Generate Signals for all live cells 0") {
+	  val liveCells = List((1, 2), (2, 3))
+	  val actualResult = List((0, 1), (0, 2), (0, 3), (1, 1), (1, 3), 
+		(2, 1), (2, 2), (2, 3), (1, 2), (1, 3), (1, 4), (2, 2),
+		(2, 4), (3, 2), (3, 3), (3, 4))
+	  val expectedResult = GameOfLife.generateSignalsForAllLiveCells(liveCells)
+	  assert(actualResult == expectedResult)
+	}
+	
+	test("Generate Signals for all live cells 1") {
+	  val liveCells = List((1, 4), (2, 3), (0, 0))
+	  val actualResult = List((0, 3), (0, 4), (0, 5), (1, 3), (1, 5),
+        (2, 3), (2, 4), (2, 5), (1, 2), (1, 3), (1, 4), (2, 2), (2, 4),
+		(3, 2), (3, 3), (3, 4), (-1, -1), (-1, 0), (-1, 1), (0, -1), 
+		(0, 1), (1, -1), (1, 0), (1, 1))
+	  val expectedResult = GameOfLife.generateSignalsForAllLiveCells(liveCells)
+	  assert(actualResult == expectedResult)
+	}
+	
+	test("Count Signals 0") {
+	  val signals = List()
+	  val actualResult = Map()
+	  val expectedResult = GameOfLife.countSignals(signals)
+	}
+	
+	test("Count Signals 1") {
+	  val signals = List((1, 2))
+	  val actualResult = Map((1, 2) -> 1)
+	  val expectedResult = GameOfLife.countSignals(signals)
+	}
+	
+	test("Count Signals 2 at same position") {
+	  val signals = List((1, 2), (1, 2))
+	  val actualResult = Map((1, 2) -> 2)
+	  val expectedResult = GameOfLife.countSignals(signals)
+	}
+	
+	test("Count Signals 3 with 2 at same position") {
+	  val signals = List((1, 2), (2, 1), (1, 2))
+	  val actualResult = Map((1, 2) -> 2, (2, 1) -> 1)
+	  val expectedResult = GameOfLife.countSignals(signals)
 	}
 	
 }
